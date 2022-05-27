66d9092e1bdf7fe81f73d55681407936d67b4444
Bryan Alex Bravo Medina <bryanalexbm@gmail.com>
2022-05-26 22:58:38 -0300

    JavaScript | Verificação do form contato | Finalizado

--- js/index.js
+++ js/index.js
@@ -22,5 +22,37 @@ function getApiGitHub() {
         })
         .catch(e => console.log(e))
 }
-
 getApiGitHub()
+
+/* VALIDAÇÂO CONTATO *////////////////////////////////////////////////////////////////////////////////////////
+const nome = document.getElementById("nome")
+const email = document.getElementById("email")
+const mensagem = document.getElementById("mensagem")
+const form = document.getElementById("form")
+const paragrafo = document.getElementById("warnings")
+
+form.addEventListener("submit", e=> {
+    e.preventDefault()
+    let warnings = ""
+    let enter = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    paragrafo.innerHTML = ""
+    if(nome.value.length <6){
+        warnings +- `O nome não é valido <br>`
+        enter = true
+    }
+    console.log(regexEmail.test(email.value))
+    if(!regexEmail.test(email.value)) {
+        warnings +- `O email não é valido <br>`
+        enter = true
+    }
+    if(mensagem.value.length <8){
+        warnings +- `O mensagen não tem suficiente conteudo <br>`
+        enter = true
+    }
+    if(enter){
+        paragrafo.innerHTML = warnings
+    }else{
+        paragrafo.innerHTML = "Mensagen eviado"
+    }
+})
